fddd400a0f08cb353af9bd1b2bf4b415b47bed71
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-04-11 14:24:56 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Product'
+import  Overlay  from './Overlay'
+function App() {
+  return (
+   <>
+   <Product />
+   <Overlay />
+   </>
+  )
+}
+
+export default App
